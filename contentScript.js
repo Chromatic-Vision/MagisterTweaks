09e7a2dc8c08edf233b632bbc4f1b912f8f6e7b5
Teru Kaazhooft <88054829+terukaaz@users.noreply.github.com>
2024-03-02 15:40:21 +0100

    Fix "G" and injected some background (WIP)

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,92 @@
 //v1.3
 
+console.log("bruh")
+
 if (typeof browser === "undefined") {
   var browser = chrome;
 }
 
-const username = "null";
-const password = "null";
+const username = null;
+const password = null;
+
+function inject() {
+
+  injectStyle(`
+  
+  h1 {
+    padding-bottom: 10px;
+    margin-bottom: 6px;
+  }
+  
+  h2 {
+    margin-top: 6px;
+  }
+  
+  h1, h2 {
+
+    background-image: linear-gradient(45deg, #f3ec78, #af4261);
+
+    background-size: 100%;
+    background-repeat: repeat;
+  
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent; 
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+    
+  }
+  
+  #injected-splash-container {
+    display: block;
+    float: right;
+    width: 50%;
+    left: 50%;
+    position: fixed;
+    top: 0;
+  }
+  
+  body {
+    background-color: #252525;
+    
+    --dna-text-color-light: #88ff88;
+    --dna-primary: #d98a30;
+    --dna-secondary: #48bf3d;
+    --dna-primary-text: var(--dna-text-color-light);
+  }
+  
+  :root {
+    --chrome-background: #333333;
+    --primary-background: #d98a30;
+    --secondary-background: #c47214;
+    --background-1: #d98a30;
+    
+  }
+   
+  .block h3 {
+    color: #33662e;
+  }
+   
+  .menu-footer {
+    background: #d98a30;
+  }
+  
+  .block .content {
+    background-color: #333333;
+  }
+  
+  `)
+
+  const bob_url = browser.runtime.getURL('assets/bob.png');
+
+  searchAndGetElementsByClassName('splash-container', (elements) => {
+    let parent;
+    for (let e of elements) {
+      parent = e.parentElement;
+      e.remove();
+    }
+    parent.innerHTML = '<img id="injected-splash-container" src=' + bob_url + '></img>'
+  })
+}
 
 function check() {
 
@@ -25,68 +106,6 @@ function check() {
 function login() {
 
   console.log("Logging in...");
-
-  addStyle(`
-
-  h1, h2 {
-
-    background-image: linear-gradient(45deg, #f3ec78, #af4261);
-
-    background-size: 100%;
-    background-repeat: repeat;
-  
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent; 
-    -moz-background-clip: text;
-    -moz-text-fill-color: transparent;
-  }
-  
-  h1 {
-    margin: 0 0 0.5rem;
-    padding: 0 0 0.5rem;
-  }
-  
-  h2 {
-    margin: 0.5rem 0 1rem;
-  }
-
-  body {
-    background-color: #333333;
-  }
-  
-  #injected-splash-container {
-    display: block;
-    float: right;
-    width: 50%;
-    left: 50%;
-    position: fixed;
-    top: 0;
-}
-
-  `)
-
-  // searchAndGetElementsByClassName("splash-container", (elements) => {
-  //   element = elements[0];
-
-  //   console.log(element);
-
-  //   console.log(element.style.background_image);
-  //   element.style.background_image = `url(/images/login_7.webp)`
-  // })
-
-  const bob_url = browser.runtime.getURL('assets/bob.png');
-
-  searchAndGetElementsByClassName('splash-container', (elements) => {
-    let parent;
-    for (let e of elements) {
-      parent = e.parentElement;
-      e.remove();
-    }
-    parent.innerHTML = '<img id="injected-splash-container" src=' + bob_url + '></img>'
-    console.log(elements);
-  })
-
-  return;
 
   searchAndGetElementById('username', (element) => {
     submit(element, username);
@@ -144,14 +163,16 @@ function submit(element, value) {
   element.dispatchEvent(event);
 }
 
-function addStyle(styleString) {
+function injectStyle(styleString) {
   const style = document.createElement('style');
   style.textContent = styleString;
   document.head.append(style);
 }
 
-if (username == null || password == null) {
-  console.error("Username or password not filled in yet! Please go to line 3 and 4 in contentScript.js and configure your username and password.");
-} else {
-  check();
-}
+// if (username == null || password == null) {
+//   console.error("Username or password not filled in yet! Please go to line 3 and 4 in contentScript.js and configure your username and password.");
+// } else {
+//   check();
+// }
+
+inject()
